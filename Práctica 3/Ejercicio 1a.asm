edba0b3c573883d13dc7e53daffd5f1e629ef0d9
fabriziogirardi <33854528+fabriziogirardi@users.noreply.github.com>
2023-09-12 21:28:41 -0300

    Actualizar Ejercicio 1a.asm

--- Práctica 3/Ejercicio 1a.asm
+++ Práctica 3/Ejercicio 1a.asm
@@ -3,7 +3,7 @@ pio_config    equ     32h                       ; Puerto de configuracion del PI
 
 ; Posición de memoria del programa principal
               org 2000h
-              mov     al, 0c3h                  ; Preparo los bits en 11000011 (1 lus prendida)
+              mov     al, 0c3h                  ; Preparo los bits en 11000011 (1 luz prendida)
               out     pio_data + 1, al          ; Pongo los datos en PB
               mov     al, 3ch                   ; Preparo la mascara en 00111100 (0 salida)
               out     pio_config + 1, al        ; Pongo la mascara en CB
